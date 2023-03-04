a4ad6be75dbbc98376b584ae0be16b53dedc4f8d
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-03-04 17:27:07 +1100

    Songs Added

--- backend/backend/Testing.py
+++ backend/backend/Testing.py
@@ -1,21 +1,47 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+# import nltk
+# from nltk.sentiment import SentimentIntensityAnalyzer
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+# # nltk.download('vader_lexicon')
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+import pandas as pd
 
-for album in albums:
-    print(album['name'])
-    
-#client id
+songs = pd.read_csv('Song_Database.csv')
+database = songs[:2]
 
-cid = 'dffd2e06079846219e33b2c574e8d893'
-secret = '3c87d80ec99d4ca88e9df68bd8821d50'
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+trained_database = []
+
+# Python values
+tempo_multiplier = 0.8
+valence_multiplier = 1
+liveness_multiplier = 0
+instrumentalness_multiplier = -1
+acousticness_multiplier = 0
+speechiness_multiplier = 0
+loudness_multiplier = 1
+energy_multiplier = 0.2
+danceability_multiplier = -0.4
+
+class Song:
+    def __init__(self, id, dance, energy, loudness, speech, acoustic, instrument, liveness, valence, tempo):
+        self.f_score = 0.1
+        self.id = id
+
+# F-value
+for index in range(0,database.count()):
+    row = database.iloc[index]
+
+# # text = 'I was asked to sign a third party contract a week out from stay. If it wasn\'t an 8 person group that took a lot of wrangling I would have cancelled the booking straight away. Bathrooms - there are no stand alone bathrooms. Please consider this - you have to clear out the main bedroom to use that bathroom. Other option is you walk through a different bedroom to get to its en-suite. Signs all over the apartment - there are signs everywhere - some helpful - some telling you rules. Perhaps some people like this but It negatively affected our enjoyment of the accommodation. Stairs - lots of them - some had slightly bending wood which caused a minor injury.'
+# happy_text = 'i had a great day'
+# sad_text = 'i did not have the greatest, most happy day ever'
+
+# sia = SentimentIntensityAnalyzer()
+# happy_results = sia.polarity_scores(happy_text)
+# sad_results = sia.polarity_scores(sad_text)
+
+# print("\n")
+# print("Happy:", happy_results['compound'])
+# print("Sad:", sad_results['compound'])
+
+# print("\nAll results")
+# print("Happy:", happy_results)
+# print("Sad:", sad_results)
