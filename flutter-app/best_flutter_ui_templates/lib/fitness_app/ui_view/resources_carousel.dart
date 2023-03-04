36bbaa485ef05a6465368cfebb6a9df3d4bf5901
s3381402 <82991172+s3381402@users.noreply.github.com>
2023-03-05 02:03:03 +1100

    fixed card carousel links

--- flutter-app/best_flutter_ui_templates/lib/fitness_app/ui_view/resources_carousel.dart
+++ flutter-app/best_flutter_ui_templates/lib/fitness_app/ui_view/resources_carousel.dart
@@ -1,46 +1,66 @@
 import 'package:flutter/material.dart';
-import '../fitness_app_theme.dart';
+import 'package:flutter_link_previewer/flutter_link_previewer.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' show PreviewData;
 import 'package:url_launcher/url_launcher.dart';
 
-class ResourcesCarousel extends StatelessWidget {
+class ResourcesCarousel extends StatefulWidget {
   final AnimationController? animationController;
   final Animation<double>? animation;
 
   ResourcesCarousel({Key? key, this.animationController, this.animation})
       : super(key: key);
 
-  final List<String> images = [
-    'https://picsum.photos/seed/picsum/300',
-    'https://picsum.photos/seed/picsum/300',
-    'https://picsum.photos/seed/picsum/300',
-    'https://picsum.photos/seed/picsum/300',
-  ];
+  @override
+  _ResourcesCarouselState createState() => _ResourcesCarouselState();
+}
 
-  final List<String> links = [
-    'https://open.spotify.com/track/2gWQbYlLaC04j3teJnFKr9',
-    'https://open.spotify.com/track/2gWQbYlLaC04j3teJnFKr9',
-    'https://open.spotify.com/track/2gWQbYlLaC04j3teJnFKr9',
-    'https://open.spotify.com/track/2gWQbYlLaC04j3teJnFKr9',
-  ];
+class _ResourcesCarouselState extends State<ResourcesCarousel> {
+
+  Map<String, PreviewData> datas = {};
+  final String article =
+      'https://greatergood.berkeley.edu/article/item/how_journaling_can_help_you_in_hard_times';
+
+  List<String> get urls {
+    return const [
+      'https://greatergood.berkeley.edu/article/item/how_journaling_can_help_you_in_hard_times',
+      'https://www.nytimes.com/2018/10/25/style/journaling-benefits.html',
+      'https://www.helpguide.org/articles/mental-health/building-better-mental-health.htm',
+      'https://www.nimh.nih.gov/health/topics/caring-for-your-mental-health',
+      'https://uhs.umich.edu/tenthings',
+      'https://www.healthywa.wa.gov.au/Articles/F_I/Good-mental-health-and-wellbeing',
+    ];
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200.0,
+      height: 400.0,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: images.length,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
             onTap: () {
               // Handle link click
-              launchUrl(links[index] as Uri);
             },
             child: Card(
               child: Column(
                 children: <Widget>[
-                  Image.network(images[index], height: 150.0),
-                  Text('Link ${index + 1}'),
+                  LinkPreview(
+                    enableAnimation: true,
+                    onPreviewDataFetched: (data) {
+                      final String url = urls[index];
+                      setState(() {
+                        datas = {
+                          ...datas,
+                          url: data,
+                        };
+                      });
+                    },
+                    previewData: datas[urls[index]],
+                    text: urls[index],
+                    width: MediaQuery.of(context).size.width * 0.8,
+                  ),
                 ],
               ),
             ),
