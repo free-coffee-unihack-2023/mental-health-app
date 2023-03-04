9d444c3bd30ee09f562435e95aeec01042a6d7e8
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-03-05 00:23:54 +1100

    Added binary search

--- backend/mentalhealthapi/views.py
+++ backend/mentalhealthapi/views.py
@@ -21,24 +21,35 @@ class MusicAPI(APIView):
 
             sorted_list = pd.read_csv('Sorted_Database.csv', encoding='latin-1')
 
-            lowest_distance = 100
-            selected_id = ''
-            selected_title = ''
-            selected_artist = ''
-            selected_score = 0
+            low = 0
+            high = len(sorted_list)
 
-            for index, row in sorted_list.iterrows():
-                id = row[0]
-                title = row[1]
-                artist = row[2]
-                song_score = row[3]
+            index = int(high / 2)
 
-                if (math.pow(math.pow(sentiment_score - song_score, 2), 0.5)) < lowest_distance:
-                    lowest_distance = sentiment_score - song_score
-                    selected_id = id
-                    selected_artist = artist
-                    selected_title = title
-                    selected_score = song_score
+            while abs(index - high) > 1 and abs(index - low) > 1:
+
+                song_score = sorted_list.iloc[index][3]
+
+                if song_score > sentiment_score:
+                    high = index
+                    index = int((high + low) / 2)
+
+                elif song_score < sentiment_score:
+                    low = index
+                    index = int((high + low) / 2)
+
+                else:
+                    break
+
+            id = sorted_list.iloc[0]
+            title = sorted_list.iloc[1]
+            artist = sorted_list.iloc[2]
+            song_score = sorted_list.iloc[3]
+
+            selected_id = id
+            selected_artist = artist
+            selected_title = title
+            selected_score = song_score
 
             url = "https://open.spotify.com/track/" + str(selected_id)
 
